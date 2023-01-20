e0ce6af43462c040dc028f9af57f772505e85209
Marouf Balideh <93919294+maroufbalideh@users.noreply.github.com>
2023-01-20 17:59:08 +0330

    flex box project first sabezlearn

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+const navBtn = document.querySelector(".nav__btn");
+const navMenu = document.querySelector(".nav-menu");
+let navOpen = false;
+navBtn.addEventListener("click", function () {
+  if (navOpen) {
+    navBtn.classList.remove("nav__btn--open");
+    navMenu.classList.remove("nav__menu--open");
+    navOpen = false;
+  } else {
+    navBtn.classList.add("nav__btn--open");
+    navMenu.classList.add("nav__menu--open");
+    navOpen = true;
+  }
+});
